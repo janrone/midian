6e34f8f522f8417c9c0a8abaddcf1b9a965e80a7
janrone <janrone@qq.com>
2019-04-05 22:18:27 +0800

    添加页面

---
+++ app/src/main/java/com/domilife/shop/adapter/CommonAdapter.kt
@@ -0,0 +1,51 @@
+package com.domilife.shop.adapter;
+
+import android.content.Context
+import android.content.Intent
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.domilife.shop.MainActivity
+import com.domilife.shop.R
+import com.domilife.shop.activity.DmWebActivity
+
+
+class CommonAdapter(context: Context?, list: ArrayList<String>?) : RecyclerView.Adapter<CommonAdapter.CommonHolder>() {
+    override fun onBindViewHolder(p0: CommonHolder, p1: Int) {
+        val title: String? = mList?.get(p1)
+
+        p0?.tvTitle?.text = title
+
+        p0.item?.setOnClickListener {
+            var context = mContext as MainActivity
+            context.startActivity(Intent(mContext, DmWebActivity::class.java),false)
+        }
+    }
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): CommonHolder {
+        return CommonHolder(mInflater?.inflate(R.layout.item_syj_layout, p0, false))
+    }
+
+
+    var mContext: Context? = null
+    private var mList: ArrayList<String>? = null
+    private var mInflater: LayoutInflater? = null
+
+    init {
+        mContext = context
+        mList = list
+        mInflater = LayoutInflater.from(context)
+    }
+
+    override fun getItemCount(): Int {
+        return mList?.size ?: 0
+    }
+
+    class CommonHolder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
+        var tvTitle: TextView = itemView?.findViewById<TextView>(R.id.tv_title) as TextView
+        var item = itemView?.findViewById<LinearLayout>(R.id.ll_item)
+    }
+}
